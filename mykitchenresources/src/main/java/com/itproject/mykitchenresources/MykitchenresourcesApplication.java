cec45fe92b9bb10954026564a1c630c67ed50524
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-11-08 01:25:37 -0600

    Resource Layer Springboot Setup

---
+++ mykitchenresources/src/main/java/com/itproject/mykitchenresources/MykitchenresourcesApplication.java
@@ -0,0 +1,13 @@
+package com.itproject.mykitchenresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykitchenresourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykitchenresourcesApplication.class, args);
+	}
+
+}
---
+++ mykitchenresources/src/test/java/com/itproject/mykitchenresources/MykitchenresourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproject.mykitchenresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykitchenresourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
