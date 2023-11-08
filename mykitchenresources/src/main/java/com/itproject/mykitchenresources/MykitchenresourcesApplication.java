6cd5eb43169b4d8f266471e07bb1cbc064f9f0de
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-11-08 01:33:09 -0600

    Maven/Angular Project Restructure + Hello World

--- mykitchenresources/src/main/java/com/itproject/mykitchenresources/MykitchenresourcesApplication.java
+++ mykitchenresources/src/main/java/com/itproject/mykitchenresources/MykitchenresourcesApplication.java
@@ -1,13 +1,21 @@
-package com.itproject.mykitchenresources;
+package com.itproject.MyKitchenResources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class MykitchenresourcesApplication {
+@RestController
+public class MyKitchenResourcesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MykitchenresourcesApplication.class, args);
+		SpringApplication.run(MyKitchenResourcesApplication.class, args);
 	}
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+    }
 
 }
--- mykitchenresources/src/test/java/com/itproject/mykitchenresources/MykitchenresourcesApplicationTests.java
+++ mykitchenresources/src/test/java/com/itproject/mykitchenresources/MykitchenresourcesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.itproject.mykitchenresources;
+package com.itproject.MyKitchenResources;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MykitchenresourcesApplicationTests {
+class MyKitchenResourcesApplicationTests {
 
 	@Test
 	void contextLoads() {
