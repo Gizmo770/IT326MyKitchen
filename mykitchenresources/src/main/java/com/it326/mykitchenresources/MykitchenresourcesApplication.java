7459e539001916de85b5814ae1cce712e3b57f38
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-11-16 22:39:14 -0600

    Created AccountService and DTO

--- mykitchenresources/src/main/java/com/it326/mykitchenresources/MykitchenresourcesApplication.java
+++ mykitchenresources/src/main/java/com/it326/mykitchenresources/MykitchenresourcesApplication.java
@@ -2,8 +2,14 @@ package com.it326.mykitchenresources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.it326.mykitchenresources.dbs.*")
+@ComponentScan(basePackages = { "com.it326.mykitchenresources.*" })
+@EntityScan("com.it326.mykitchenresources.*")  
 public class MykitchenresourcesApplication {
 
 	public static void main(String[] args) {
---
+++ mykitchenresources/src/main/java/com/it326/mykitchenresources/services/AccountService.java
@@ -0,0 +1,31 @@
+package com.it326.mykitchenresources.services;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+import com.it326.mykitchenresources.dbs.AccountDb;
+import com.it326.mykitchenresources.entities.Account;
+
+@Service
+public class AccountService {
+
+    private final AccountDb accountDb;
+
+    public AccountService(AccountDb accountDb) {
+        this.accountDb = accountDb;
+    }
+
+    public Account createAccount(String name, String username, String password) {
+        Account account = new Account();
+        account.setName(name);
+        account.setUserName(username);
+        account.setHashedPassword(hashPassword(password));
+        return accountDb.save(account);
+    }
+
+    private String hashPassword(String password) {
+        // Generate a salt and hash the password using BCrypt
+        String salt = BCrypt.gensalt();
+        return BCrypt.hashpw(password, salt);
+    }
+}
