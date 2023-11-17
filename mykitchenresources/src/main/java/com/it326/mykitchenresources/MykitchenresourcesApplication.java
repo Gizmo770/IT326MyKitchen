859c2f6c856a8de5cf4bc9072d5963cf0857acf5
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-11-16 21:22:10 -0600

    Created Account Class Entity

--- mykitchenresources/src/main/java/com/it326/mykitchenresources/MykitchenresourcesApplication.java
+++ mykitchenresources/src/main/java/com/it326/mykitchenresources/MykitchenresourcesApplication.java
@@ -2,10 +2,8 @@ package com.it326.mykitchenresources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.it326.mykitchenresources.entities")
 public class MykitchenresourcesApplication {
 
 	public static void main(String[] args) {
---
+++ mykitchenresources/src/main/java/com/it326/mykitchenresources/entities/Account.java
@@ -0,0 +1,61 @@
+package com.it326.mykitchenresources;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")   
+    private int accountId;
+
+    @Column(name = "name", length = 45)
+    private String name;
+
+    @Column(name = "username", length = 45)
+    private String username;
+
+    @Column(name = "hashed_password", length = 45)
+    private String hashedPassword;
+    
+    // Constructors, getters, setters, etc.
+    public Account(){}
+
+    public Account(int accountId, String name, String username, String hashedPassword){
+        this.accountId = accountId;
+        this.name = name;
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+    }
+    
+    public int getAccountId() {
+        return accountId;
+    }
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUserName() {
+        return username;
+    }
+    public void setUserName(String username) {
+        this.username = username;
+    }
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+}
