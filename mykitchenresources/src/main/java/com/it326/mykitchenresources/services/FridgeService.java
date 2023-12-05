dd6f7d40a8a0b074cce11e8d8016a52ba045b441
basho-o <145047799+basho-o@users.noreply.github.com>
2023-12-05 13:19:55 -0600

    Update FridgeService.java

--- mykitchenresources/src/main/java/com/it326/mykitchenresources/services/FridgeService.java
+++ mykitchenresources/src/main/java/com/it326/mykitchenresources/services/FridgeService.java
@@ -61,4 +61,57 @@ public class FridgeService {
     public Fridge getFridgeByAccountId(Integer accountId) {
         return fridgeDb.findByAccount(accountService.findByAccountId(accountId));
     }
+
+      // Search for an ingredient by name
+    public Ingredient searchIngredient(String name) {
+        for (Ingredient ingredient : ingredients) {
+            if (ingredient.toString().equalsIgnoreCase(name)) {
+                return ingredient;
+            }
+        }
+        return null; // Ingredient not found
+    }
+
+    // Search for a recipe by name
+    public Recipe searchRecipe(String name) {
+        for (Recipe recipe : recipes) {
+            if (recipe.toString().equalsIgnoreCase(name)) {
+                return recipe;
+            }
+        }
+        return null; // Recipe not found
+    }
+    // Filter search results
+    public List<Ingredient> filterSearchResults(List<Ingredient> results, String filter) {
+        List<Ingredient> filteredResults = new ArrayList<>();
+        for (Ingredient ingredient : results) {
+            if (ingredient.toString().toLowerCase().contains(filter.toLowerCase())) {
+                filteredResults.add(ingredient);
+            }
+        }
+        return filteredResults;
+    }
+
+    // Provide search suggestions based on input
+    public List<String> getSearchSuggestions(String input) {
+        List<String> suggestions = new ArrayList<>();
+        // Logic to generate suggestions based on input
+      
+        return suggestions;
+    }
+
+    // Update an existing ingredient
+    public void updateIngredient(Ingredient oldIngredient, Ingredient newIngredient) {
+        int index = ingredients.indexOf(oldIngredient);
+        if (index != -1) {
+            ingredients.set(index, newIngredient);
+        }
+    }
+
+    // Delete an existing ingredient
+    public void deleteIngredient(Ingredient ingredient) {
+        ingredients.remove(ingredient);
+    }
+
+
 }
