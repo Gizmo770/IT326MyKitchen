72059c72d966274b0897d9a5af9c17002ffdbe5c
Ben Williams <benwilliams2000@gmail.com>
2023-12-05 12:23:13 -0600

    Update 1

--- AngularApp/MyKitchen/src/app/app-routing.module.ts
+++ AngularApp/MyKitchen/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FridgeComponent } from './fridge/fridge.component';
+import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/fridge', pathMatch: 'full' },
+  { path: 'fridge', component: FridgeComponent },
+  { path: 'recipe-search', component: RecipeSearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- AngularApp/MyKitchen/src/app/app.module.ts
+++ AngularApp/MyKitchen/src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardModule } from 'primeng/card';
 
 import { ButtonModule } from 'primeng/button'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FridgeComponent } from './fridge/fridge.component';
+import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FridgeComponent,
+    RecipeSearchComponent
   ],
   imports: [
     // PrimeNG Modules
     ButtonModule,
+    CardModule,
     // Angular Modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
