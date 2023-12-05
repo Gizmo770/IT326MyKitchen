cb75956b82bdccfe9bb19ce3499b83d1346691cf
basho-o <145047799+basho-o@users.noreply.github.com>
2023-12-05 13:39:21 -0600

    Update FridgeService.java
    Added the methods discussed in teams

--- mykitchenresources/src/main/java/com/it326/mykitchenresources/services/FridgeService.java
+++ mykitchenresources/src/main/java/com/it326/mykitchenresources/services/FridgeService.java
@@ -62,56 +62,44 @@ public class FridgeService {
         return fridgeDb.findByAccount(accountService.findByAccountId(accountId));
     }
 
-      // Search for an ingredient by name
-    public Ingredient searchIngredient(String name) {
-        for (Ingredient ingredient : ingredients) {
-            if (ingredient.toString().equalsIgnoreCase(name)) {
-                return ingredient;
+    public List<Ingredient> searchIngredient(Integer accountId, String searchQuery) {
+    Fridge fridgeData = getFridgeByAccountId(accountId);
+
+    List<Ingredient> searchResults = new ArrayList<>();
+    for (Ingredient ingredient : fridgeData.getIngredients()) {
+        if (ingredient.getName().toLowerCase().contains(searchQuery.toLowerCase())) {
+            searchResults.add(ingredient);
+        }
+    }
+
+    return searchResults;
+}
+
+public void updateIngredient(Integer accountId, Long ingredientId, String newName, Double newQuantity, String newExpDate) {
+    Fridge fridgeData = getFridgeByAccountId(accountId);
+
+    for (Ingredient ingredient : fridgeData.getIngredients()) {
+        if (ingredient.getId().equals(ingredientId)) {
+            ingredient.setName(newName);
+            ingredient.setQuantity(newQuantity);
+            try {
+                Date convertedDate = new SimpleDateFormat("yyyy-MM-dd").parse(newExpDate);
+                ingredient.setExpirationDate(convertedDate);
+            } catch (ParseException e) {
+                System.out.println("Error converting date");
             }
-        }
-        return null; // Ingredient not found
-    }
-
-    // Search for a recipe by name
-    public Recipe searchRecipe(String name) {
-        for (Recipe recipe : recipes) {
-            if (recipe.toString().equalsIgnoreCase(name)) {
-                return recipe;
-            }
-        }
-        return null; // Recipe not found
-    }
-    // Filter search results
-    public List<Ingredient> filterSearchResults(List<Ingredient> results, String filter) {
-        List<Ingredient> filteredResults = new ArrayList<>();
-        for (Ingredient ingredient : results) {
-            if (ingredient.toString().toLowerCase().contains(filter.toLowerCase())) {
-                filteredResults.add(ingredient);
-            }
-        }
-        return filteredResults;
-    }
-
-    // Provide search suggestions based on input
-    public List<String> getSearchSuggestions(String input) {
-        List<String> suggestions = new ArrayList<>();
-        // Logic to generate suggestions based on input
-      
-        return suggestions;
-    }
-
-    // Update an existing ingredient
-    public void updateIngredient(Ingredient oldIngredient, Ingredient newIngredient) {
-        int index = ingredients.indexOf(oldIngredient);
-        if (index != -1) {
-            ingredients.set(index, newIngredient);
+            break;
         }
     }
 
-    // Delete an existing ingredient
-    public void deleteIngredient(Ingredient ingredient) {
-        ingredients.remove(ingredient);
-    }
+    fridgeDb.save(fridgeData);
+}
 
+public void deleteIngredient(Integer accountId, Long ingredientId) {
+    Fridge fridgeData = getFridgeByAccountId(accountId);
 
+    fridgeData.getIngredients().removeIf(ingredient -> ingredient.getId().equals(ingredientId));
+
+    fridgeDb.save(fridgeData);
+}
 }
